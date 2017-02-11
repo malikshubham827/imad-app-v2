b48c239619b28ca3478dc1c78c4d77a70c5575d6
malikshubham827 <malikshubham827@gmail.com>
2017-02-12 00:53:04 +0530

    Adding /article-xx routes

--- server.js
+++ server.js
@@ -5,20 +5,48 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+let articles = {
+  'article-one': {
+  heading: 'This is article-one heading',
+  text: 'This is article-one text'
+}, 'article-two': {
+  heading: 'This is article-two heading',
+  text: 'This is article-two text'
+}, 'article-three': {
+  heading: 'This is article-three heading',
+  text: 'This is article-three heading'
+}};
+
+let htmlTemplate = function(data) {
+  return `
+  <h2>${data.heading}</h2>
+  <p>${data.text}</p>
+  `
+}
+
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+// app.get('/ui/article-one', function(req, res) {
+//   res.sendFile(__dirname, 'ui', 'article-one.html');
+// });
+
+app.get('/:name', function(req, res) {
+  let articleName = req.params.name;
+  res.send(htmlTemplate(articles[articleName]));
+});
+
+app.get('/ui/style.css', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/madi.png', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+app.listen(8080, function() {
   console.log(`IMAD course app listening on port ${port}!`);
 });
